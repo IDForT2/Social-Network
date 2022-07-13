540500c7951d7707903d43659f50db879054450e
Danil <madcobra918@gmail.com>
2022-07-13 15:01:29 +0300

    v0.2.2
    - Got rid of Side Effect in Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,18 +3,22 @@ import * as axios from "axios";
 import userPhoto from "../../assets/images/user.png"
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small != null ? u.photo.small: userPhoto} className={classes.usersPhoto}/>
+                        <img src={u.photos.small != null ? u.photo.small : userPhoto} className={classes.usersPhoto}/>
                     </div>
                     <div>
                         {u.isFollowed
