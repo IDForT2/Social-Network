bf0ec1ea16f4a146b359e134f3b794def392c6c9
Danil <madcobra918@gmail.com>
2022-07-17 14:42:59 +0300

    v0.3.2 - DAL
    - Added DAL

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        "API-KEY": "63205f57-4b4c-4377-a3d9-c12d2cddc407"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`, {
+            withCredentials: true
+        }).then(response => response.data);
+    }
+}
+
+
