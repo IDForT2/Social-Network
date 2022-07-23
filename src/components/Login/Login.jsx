5d19dc0cc068731bc05db70fa1229460cf2885df
Danil <madcobra918@gmail.com>
2022-07-23 17:02:58 +0300

    v0.4.9 - Handling Login Errors
    - Implemented handling of errors that occur during authorization (stopSubmit).

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
 import {Navigate} from "react-router-dom";
+import classes from "./../common/FormsControl/FormControls.module.css";
 
 const LoginForm = (props) => {
     return (
@@ -20,6 +21,9 @@ const LoginForm = (props) => {
             <div>
                 <Field type={"checkbox"} name={"rememberMe"} component={Input} /> remember me
             </div>
+            {props.error && <div className={classes.formSummaryError}>
+                {props.error}
+            </div>}
             <div>
                 <button>Login</button>
             </div>
