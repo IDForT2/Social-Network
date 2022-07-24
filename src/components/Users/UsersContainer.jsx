f2733c8052d5223e8472c6df9042e337d2ea09d7
Danil <madcobra918@gmail.com>
2022-07-24 14:10:47 +0300

    v0.4.11 - Refactoring and Bug fix
    -Fixed a bug with displaying the current page on the Users page

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,6 @@
 import {connect} from "react-redux";
 import {
-    follow, getUsers,
+    follow, requestUsers,
     setCurrentPage,
     toggleFollowingProgress,
     unfollow
@@ -9,6 +9,13 @@ import React from "react";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
 import {compose} from "redux";
+import {
+    getCurrentPage,
+    getFollowingInProgress,
+    getIsFetching,
+    getPageSize,
+    getTotalUsersCount, getUsers
+} from "../../redux/Selectors/users-selectors";
 
 
 class UsersContainer extends React.Component {
@@ -39,12 +46,12 @@ class UsersContainer extends React.Component {
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress
+        users: getUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state)
     }
 }
 
@@ -57,5 +64,5 @@ export default compose(
             unfollow,
             setCurrentPage,
             toggleFollowingProgress,
-            getUsers
+            getUsers: requestUsers
         }))(UsersContainer);
