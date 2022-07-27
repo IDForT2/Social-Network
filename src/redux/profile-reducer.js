1cf9e6aa7d0f5207f4fbf8da7ed87735fba8f1dd
Danil <madcobra918@gmail.com>
2022-07-27 19:45:29 +0300

    v0.5.3 - Unit testing and tdd
    -Added Unit tests and tdd in profile-reducer modul

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import {profileAPI, usersAPI} from "../api/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
 
 let initialState = {
     posts: [
@@ -40,6 +41,9 @@ const profileReducer = (state = initialState, action) => {
         case SET_STATUS: {
             return {...state, status: action.status}
         }
+        case DELETE_POST: {
+            return {...state, posts: state.posts.filter(p => p.id != action.postId)}
+        }
         default:
             return state;
     }
@@ -57,6 +61,11 @@ export const setUserProfile = (profile) => {
 export const setStatus = (status) => {
 
     return ({type: SET_STATUS, status});
+}
+
+export const deletePost = (postId) => {
+
+    return ({type: DELETE_POST, postId});
 }
 
 export const getUserProfile = (userId) => (dispatch) => {
