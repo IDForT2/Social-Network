42ea9bf35a00933de0edd304cbb24861fa333381
Danil <madcobra918@gmail.com>
2022-07-31 20:01:04 +0300

    v0.5.9 - Bundling
    -Added React.lazy and React.Suspense functions in App.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import React from "react";
 import './App.css';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import SuperNavbarContainer from "./components/Navbar/NavbarContainer";
-import SuperDialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Login/Login";
 import {Component} from "react";
 import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
 import {compose} from "redux";
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
+const SuperDialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const Login = React.lazy(() => import('./components/Login/Login'));
 
 class App extends Component {
     componentDidMount() {
@@ -31,18 +32,20 @@ class App extends Component {
                 <HeaderContainer/>
                 <SuperNavbarContainer/>
                 <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path="/profile/:userId"
-                               element={<ProfileContainer/>}/>
-                        <Route path="/profile/"
-                               element={<ProfileContainer/>}/>
-                        <Route path="/dialogs/*" element={<SuperDialogsContainer/>}/>
-                        <Route path="/news" element={<News/>}/>
-                        <Route path="/music" element={<Music/>}/>
-                        <Route path="/settings" element={<Settings/>}/>
-                        <Route path="/users" element={<UsersContainer/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                    </Routes>
+                    <React.Suspense fallback={<div><Preloader/></div>}>
+                        <Routes>
+                            <Route path="/profile/:userId"
+                                   element={<ProfileContainer/>}/>
+                            <Route path="/profile/"
+                                   element={<ProfileContainer/>}/>
+                            <Route path="/dialogs/*" element={<SuperDialogsContainer/>}/>
+                            <Route path="/news" element={<News/>}/>
+                            <Route path="/music" element={<Music/>}/>
+                            <Route path="/settings" element={<Settings/>}/>
+                            <Route path="/users" element={<UsersContainer/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                        </Routes>
+                    </React.Suspense>
                 </div>
             </div>
         );
@@ -59,7 +62,7 @@ let AppContainer = compose(
 const SocialNetworkApp = (props) => {
     return <BrowserRouter>
         <Provider store={store}>
-            <AppContainer />
+            <AppContainer/>
         </Provider>
     </BrowserRouter>
 }
